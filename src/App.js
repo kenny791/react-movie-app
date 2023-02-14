b88c51257c6b92a59ca6a2e2ec02397060f7bb9e
Kenny Lai <3154018+kenny791@users.noreply.github.com>
2023-02-14 14:06:49 +1100

    feed MovieCard component with state set from api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import './App.css'
 import SearchIcon from "./search.svg"
 import MovieCard from "./MovieCard"
 
 const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=fe9176dd"
 
-const movie ={
-    "Title": "The Avengers",
-    "Year": "2012",
-    "imdbID": "tt0848228",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg"
-}
+// const movie ={
+//     "Title": "The Avengers",
+//     "Year": "2012",
+//     "imdbID": "tt0848228",
+//     "Type": "movie",
+//     "Poster": "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg"
+// }
 
 const App = () => {
+    const [movies, setMovies] = useState([])
 
     const searchMovies =async (title) => {
         const reponse = await fetch (`${API_URL}&s=${title}`)
         const data = await reponse.json()
-        console.log(data.Search)
+        setMovies(data.Search)
     }
 
     useEffect(() => {
@@ -43,7 +44,7 @@ const App = () => {
                 />
             </div>
             <div className="container">
-                <MovieCard movie={movie} />
+                <MovieCard movie={movies[0]} />
             </div>
 
         </div>
