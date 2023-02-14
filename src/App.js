0bbbc0e3af5e81315fb5de9a33b7d7eaef29e8cf
Kenny Lai <3154018+kenny791@users.noreply.github.com>
2023-02-14 13:36:56 +1100

    add movie year and poster

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import SearchIcon from "./search.svg"
 
 const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=fe9176dd"
 
+const movie1 ={
+    "Title": "The Avengers",
+    "Year": "2012",
+    "imdbID": "tt0848228",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg"
+}
+
 const App = () => {
 
     const searchMovies =async (title) => {
@@ -34,6 +42,17 @@ const App = () => {
                     onClick={() => {}}
                 />
             </div>
+            <div className="container">
+                <div className="movie">
+                    <div>
+                        <p>{movie1.Year}</p>
+                    </div>
+                    <div>
+                        <img src={movie1.Poster !== "N/A"? movie1.Poster : "https://via.placeholder.com/400"} alt={movie1.Title} />
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 }
