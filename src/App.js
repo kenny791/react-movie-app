a211784c41ce43dde4b2fe75e3b9775aa22e88e6
Kenny Lai <3154018+kenny791@users.noreply.github.com>
2023-02-14 12:47:01 +1100

    fetch data from omdb API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react"
+
+
+
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=fe9176dd"
 
 const App = () => {
+
+    const searchMovies =async (title) => {
+        const reponse = await fetch (`${API_URL}&s=${title}`)
+        const data = await reponse.json()
+        console.log(data.Search)
+    }
+
+    useEffect(() => {
+        searchMovies("avengers")
+    }, [])
+
+
+
     return (
         <div>
         <h1>Hello World</h1>
@@ -8,4 +25,4 @@ const App = () => {
     );
 }
 
-export default App;
+export default App
